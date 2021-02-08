be2d39902df66b1a579efcc8ffbb11997a48c751
HOME-PC\Valera <Cagema.erm@gmail.com>
2021-02-08 11:39:13 +0300

    Create BoardManager

---
+++ Puzzle_Seed/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    public static BoardManager S;
+    public List<Sprite> gems = new List<Sprite>();
+    public GameObject tile;
+    public int xSize, ySize;
+    private GameObject[,] tiles;
+    public bool isShifting { get; set; }
+
+    private void Start()
+    {
+        S = GetComponent<BoardManager>();
+        Vector2 offset = tile.GetComponent<SpriteRenderer>().bounds.size;
+        CreateBoard(offset.x, offset.y);
+    }
+
+    private void CreateBoard(float xOffset, float yOffset)
+    {
+        tiles = new GameObject[xSize, ySize];
+        float startX = transform.position.x;
+        float startY = transform.position.y;
+
+        Sprite[] previousLeft = new Sprite[ySize];
+        Sprite previousBelow = null;
+
+        for (int x = 0; x < xSize; x++)
+        {
+            for (int y = 0; y < ySize; y++)
+            {
+                GameObject newTile = Instantiate(
+                    tile,
+                    new Vector3(startX + (xOffset * x), startY + (yOffset * y), 0),
+                    tile.transform.rotation);
+                tiles[x, y] = newTile;
+
+                newTile.transform.parent = transform;
+
+                List<Sprite> possibleGems = new List<Sprite>();
+                possibleGems.AddRange(gems);
+                possibleGems.Remove(previousLeft[y]);
+                possibleGems.Remove(previousBelow);
+
+                Sprite newSprite = possibleGems[Random.Range(0, possibleGems.Count)];
+                newTile.GetComponent<SpriteRenderer>().sprite = newSprite;
+                newTile.name = newSprite.name;
+
+                previousLeft[y] = newSprite;
+                previousBelow = newSprite;
+            }
+        }
+    }
+}
---
+++ Puzzle_Seed/Assets/Scripts/Tile.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+	private static Color selectedColor = new Color(.5f, .5f, .5f, 1.0f);
+	private static Tile previousSelected = null;
+
+	private SpriteRenderer render;
+	private bool isSelected = false;
+
+	private Vector2[] adjacentDirections = new Vector2[] { Vector2.up, Vector2.down, Vector2.left, Vector2.right };
+
+	void Awake()
+	{
+		render = GetComponent<SpriteRenderer>();
+	}
+
+	private void Select()
+	{
+		isSelected = true;
+		render.color = selectedColor; // Change color selected sprite
+		previousSelected = gameObject.GetComponent<Tile>();
+		// *Sound* SFXManager.S.PlaySFX(Clip.Select);
+	}
+
+	private void Deselect()
+	{
+		isSelected = false;
+		render.color = Color.white;
+		previousSelected = null;
+	}
+
+    private void OnMouseDown()
+    {
+		if (render.sprite == null || BoardManager.S.isShifting)
+        {
+			return;
+        }
+
+		if (isSelected)
+        {
+			// Already selected?
+			Deselect();
+        }
+        else
+        {
+			if (previousSelected == null)
+            {
+				// it is first tile selected
+				Select();
+            }
+            else
+            {
+                if (GetAllAdjacentTiles().Contains(previousSelected.gameObject))
+                {
+					SwapSprite(previousSelected.render);
+					previousSelected.Deselect();
+                }
+                else
+                {
+					// Select not adjacent sprite
+					previousSelected.Deselect();
+					Select();
+                }
+            }
+        }
+    }
+
+	public void SwapSprite(SpriteRenderer secondRender)
+    {
+		if (render.sprite == secondRender.sprite)
+        {
+			// Don't swap same sprites
+			return;
+        }
+
+		Sprite tempSprite = secondRender.sprite;
+		secondRender.sprite = render.sprite;
+		render.sprite = tempSprite;
+		// *Sound* SFXManager.S.PlaySFX(Clip.Swap);
+    }
+
+	private GameObject GetAdjacent(Vector2 castDir)
+    {
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, castDir);
+		if (hit.collider != null)
+        {
+			return hit.collider.gameObject;
+        }
+		return null;
+    }
+
+	private List<GameObject> GetAllAdjacentTiles()
+    {
+		List<GameObject> adjacentTiles = new List<GameObject>();
+		for (int i = 0; i < adjacentDirections.Length; i++)
+        {
+			adjacentTiles.Add(GetAdjacent(adjacentDirections[i]));
+        }
+		return adjacentTiles;
+    }
+}
